a2cb16352bd962ece7cb4c56c8f6b71d94e1946e
Christian Buntin <christian.buntin@student.kit.edu>
2012-05-11 17:50:17 +0200

    Algorithmus zum Zeichnen einer Linie hinzugefügt

--- simple-drawer/line.cpp
+++ simple-drawer/line.cpp
@@ -1,6 +1,7 @@
 #include "line.h"
 
 #include <cmath>
+#include <iostream>
 
 
 namespace bmp
@@ -13,8 +14,23 @@ namespace bmp
 		}
 
 
-		// please IMPLEMENT here!!
+		int delta_X = std::abs((int)p_from.getX() - (int)p_to.getX());
+		int delta_Y = std::abs((int)p_from.getY() - (int)p_to.getY());
 
+		int Number_of_Steps = delta_Y > delta_X ? delta_Y : delta_X;
+
+		double dX = delta_X*1.0/Number_of_Steps;
+		double dY = delta_Y*1.0/Number_of_Steps;
+
+		//std::cout << "X: " << p_to.getX() << " Y: " << p_to.getY() << std::endl;
+		//std::cout << "Number: " << Number_of_Steps << std::endl;
+
+		for(int i=0;i<=Number_of_Steps;i++) {
+			p_target.setPixel((int)(i*dX + p_from.getX()+0.5), (int)(i*dY + p_from.getY()+0.5), p_color);
+		}
+		//p_target.setPixel(p_from.getX(), p_from.gety(), p_color);
+
+		// please IMPLEMENT here!!
 
 		return true;
 	}
